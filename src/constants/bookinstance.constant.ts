ffc707c894ea515ca575acdda10df39df67ef320
ctpanh <ctpanh03@gmail.com>
2024-07-18 16:59:50 +0700

    Part 4: Template primer

---
+++ src/constants/bookinstance.constant.ts
@@ -0,0 +1,7 @@
+export enum BOOK_INSTANCE_STATUS {
+  AVAILABLE = "Available",
+  UNAVAILABLE = "Unavailable",
+  MAINTENANCE = "Maintenance",
+  RESERVED = "Reserved",
+  LOANED = "Loaned",
+}
---
+++ src/services/index.ts
@@ -0,0 +1,4 @@
+export * from "./author.service";
+export * from "./book.service";
+export * from "./bookinstance.service";
+export * from "./genre.service";
